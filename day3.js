0e5a377dbb0f26a737ea3be119a43ae2d7a1c754
Nuzair Ibrahim <nuzairb@gmail.com>
2018-11-04 15:12:04 +0500

    init
    init

---
+++ day3.js
@@ -0,0 +1,31 @@
+// let catName = "Fluffy"
+// let catAge = 4
+
+// function meow() {
+//   alert("Meoooowwwwww")
+// }
+/*
+let cat = {
+  name: "Fluffy",
+  age: 4,
+  foods: {
+    favorite: "Tuna",
+    leastFavorite: "Oranges"
+  },
+  eyeColor: "blue",
+  meow() {
+    alert("Meoowwww")
+  }
+}
+console.log(cat.name)
+console.log(cat.age)
+console.log(cat.eyeColor)
+console.log(cat.meow())
+console.log(cat.foods.favorite)
+console.log(cat.foods.leastFavorite)
+*/
+
+document.addEventListener("click", myAmazingFunction) 
+function myAmazingFunction() {
+  alert("Hello there!")
+}
