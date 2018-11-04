13eca6d02a264377e2be87bd0272ff4c47d65eeb
Nuzair Ibrahim <nuzairb@gmail.com>
2018-11-04 15:13:20 +0500

    init
    init

---
+++ day5.js
@@ -0,0 +1,13 @@
+// let strawberryCount = 20
+
+// if (strawberryCount != 9) {
+//   document.write("Congrats!")  
+// } else {
+//   document.write("Sorry, we do not ship orders that small.")  
+// }
+
+let strawberryCount = 2
+while (strawberryCount <= 200) {
+  document.write("There are" + strawberryCount + "strawberies.<br>")
+  strawberryCount++
+}
